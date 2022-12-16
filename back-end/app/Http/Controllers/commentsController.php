6dff89ad2cd756f640159cfaab9fd0e9fc4fa292
AmineJml <aminejamal003@gmail.com>
2022-12-16 14:15:11 +0200

    adding apis to commentsController

--- back-end/app/Http/Controllers/commentsController.php
+++ back-end/app/Http/Controllers/commentsController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class commentsController extends Controller
 {
-    //
+    function add_comment(Request $request){
+        $comment = new Comment;
+        $comment->post_id = $request->post_id;
+        $comment->user_id = $request->user_id;
+        $comment->comment = $request->comment;
+
+        $validator = Validator::make($request->all(), [
+            'post_id' => 'required',
+            'user_id' => 'required',
+            'comment' => 'required',
+        ]);
+        //validator infinite loop
+        if($validator->fails()) {
+            return response()->json([
+                "result" => "false" 
+            ]); 
+        }
+
+        if($comment->save()){
+            return response()->json([
+                "result" => "true" 
+            ]);
+        }    
+    }
 }
