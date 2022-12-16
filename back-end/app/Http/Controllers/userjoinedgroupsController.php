ac4e53f8b7ec641e5bb3019a035daf1cffbd8ec6
AmineJml <aminejamal003@gmail.com>
2022-12-16 14:16:18 +0200

    adding apis to userjoinedgroupsController

--- back-end/app/Http/Controllers/userjoinedgroupsController.php
+++ back-end/app/Http/Controllers/userjoinedgroupsController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class userjoinedgroupsController extends Controller
 {
-    //
+    function join_group(Request $request){
+
+        $UserJoinGroup = new UserJoinedGroup;
+        $UserJoinGroup->group_id = $request->group_id;
+        $UserJoinGroup->user_id = $request->user_id;
+        $UserJoinGroup->is_joined = $request->is_joined;
+
+        $validator = Validator::make($request->all(), [
+            'group_id' => 'required',
+            'user_id' => 'required',
+            'is_joined' => 'required',
+        ]);
+        //validator infinite loop
+        if($validator->fails()) {
+            return response()->json([
+                "result" => "false_auth" 
+            ]); 
+        }
+
+        $check_UserJoinGroup = UserJoinedGroup::where('group_id',  $UserJoinGroup->group_id)
+                                            ->where('user_id',  $UserJoinGroup->user_id)
+                                            ->get();
+        if(!$check_UserJoinGroup){
+            $UserJoinGroup = UserJoinedGroup::where('group_id',  $UserJoinGroup->group_id)
+                                          ->where('user_id',  $UserJoinGroup->user_id)
+                                          ->update(['is_joined'=>  $UserJoinGroup->is_joined]);
+            return response()->json([
+                "result" => "false" 
+            ]);    
+        }
+        if($UserJoinGroup->save()){
+            return response()->json([
+                "result" => "true" 
+            ]);
+        } 
+    }
 }
