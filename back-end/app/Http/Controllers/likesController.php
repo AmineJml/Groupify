5aafffd46c17e3e99437d73c2ee2c60158546ea2
AmineJml <aminejamal003@gmail.com>
2022-12-16 14:15:41 +0200

    adding apis to likesController

--- back-end/app/Http/Controllers/likesController.php
+++ back-end/app/Http/Controllers/likesController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class likesController extends Controller
 {
-    //
+    function like_post(Request $request){
+        $like = new Like;
+        $like->post_id = $request->post_id;
+        $like->user_id = $request->user_id;
+        $like->is_liked = $request->is_liked;
+
+        $validator = Validator::make($request->all(), [
+            'post_id' => 'required',
+            'user_id' => 'required',
+            'is_liked' => 'required',
+        ]);
+        //validator infinite loop
+        if($validator->fails()) {
+            return response()->json([
+                "result" => "false" 
+            ]); 
+        }
+
+        $check_like = Like::where('post_id',  $like->post_id)
+                    ->where('user_id', $like->user_id)
+                    ->get();
+        if($check_like){
+            $like = Like::where('post_id',  $like->post_id)
+                         ->where('user_id', $like->user_id)
+                         ->update(['is_liked'=> $like->is_liked]);
+            return response()->json([
+                "result" => "like_is_found" 
+            ]);    
+        }
+        if($like->save()){
+            return response()->json([
+                "result" => "true" 
+            ]);
+        } 
+    }
 }
